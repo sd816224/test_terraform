05317418e3b234bdcb2d0e8380141f6749ab85b3
Alex Chan <alextfchan96@gmail.com>
2023-11-01 12:26:58 +0000

    completed get_connection util and testing

--- src/ingestion_lambda/OLTP_get_connection.py
+++ src/ingestion_lambda/OLTP_get_connection.py
@@ -3,30 +3,23 @@ from pg8000.native import Connection, InterfaceError, DatabaseError, Error
 from dotenv import load_dotenv
 import logging
 
-load_dotenv()
+
 logger = logging.getLogger('MyLogger')
 logger.setLevel(logging.INFO)
 
-
-user=os.environ['PGUSER']
-host=os.environ['PGHOST']
-port=os.environ['PGPORT']
-database=os.environ['DB']
-password=os.environ['PGPASSWORD']
-
-def get_connection():
+def get_connection(user, host, database, port, password):
     '''
     This utility function allows for connections to the OLTP database - Totesys.
     '''
     try:
-        conn = Connection(user, host, database, port, password)
+        load_dotenv()
         logger.info('Connection to database Totesys has been established.')
-        return conn
-        
+        return Connection(user, host, database, port, password)
+
     except (InterfaceError, DatabaseError) as e:
-        logger.error(f"pg8000 - {e} has occured.")
-        return e
+        logger.error('pg8000 - an error has occured.')
+        raise e
     except Exception as exc:
         logger.error('An error has occured when attempting to connect to the database.')
         raise exc
-    
+
--- test/test_OLTP_get_connection.py
+++ test/test_OLTP_get_connection.py
@@ -1,42 +1,55 @@
 import os
 import logging
 import pytest
-from src.utils.OLTP_get_connection import get_connection
+from dotenv import load_dotenv
+from pg8000.native import Connection, InterfaceError, DatabaseError
+from src.ingestion_lambda.OLTP_get_connection import get_connection
+
 
 logger = logging.getLogger('MyLogger')
-logger.setLevel(logging.INFO)
 
-
-# def test_get_connection_returns_correct_print(capsys):
-#     '''
-#     Testing get_connection function. Function, if successful, will print:
-#     "Connection to database Totesys has been established."
-#     '''
-#     get_connection()
-#     captured = capsys.readouterr()
-#     all_outputs=captured.out.split('\n')
-#     last_outputs=all_outputs[0]
-#     expected = 'Connection to database Totesys has been established.'
-#     print(f'Result is: {last_outputs}')
-#     assert last_outputs == expected
 
 def test_get_connection_returns_correct_log_when_successful_connection(caplog):
-    logger.info('Connection to database Totesys has been established.')
+    '''
+    This test should return the correct log in CloudWatch, when passed correct details
+    Requirements:
+        .env (using dotenv.load_dotenv module) file with credentials for accessing a database.
+        credentials required are: user, host, port, database, and password.
+    '''
     with caplog.at_level(logging.INFO):
-        get_connection()
+        load_dotenv()
+        user=os.environ['PGUSER']
+        host=os.environ['PGHOST']
+        port=os.environ['PGPORT']
+        database=os.environ['DB']
+        password=os.environ['PGPASSWORD']
+        get_connection(user, host, database, port, password)
         assert('Connection to database Totesys has been established.' in caplog.text)
 
-@pytest.fixture(scope='session')
-def test_get_connection_with_failed_connection():
-    db_settings={
-        'user'            : 'postgres',
-        'host'            : 'test',
-        'port'            : '5432',
-        'database'        : 'test_database',
-        'password'        : '',
-    }
-    
+
+def test_get_connection_with_database_error():
+    '''
+    Testing for a DatabaseError. When passed incorrect details, should return DatabaseError.
+    '''
+    with pytest.raises(DatabaseError):
+        load_dotenv()      
+        user=os.environ['PGUSER']
+        host=os.environ['PGHOST']
+        port=os.environ['PGPORT']
+        database=os.environ['DB']
+        password=''
+        get_connection(user, host, database, port, password)
 
 
-# def test_get_connection_returns_correct_query_response():
-#     pass
+def test_get_connection_with_unexpected_error():
+    '''
+    When passed an unexpected error, i.e. no password credentials passed, will return an error.
+    '''
+    with pytest.raises(Exception):
+        load_dotenv()      
+        user=os.environ['PGUSER']
+        host=os.environ['PGHOST']
+        port=os.environ['PGPORT']
+        database=os.environ['DB']
+        get_connection(user, host, database, port, password)
+
