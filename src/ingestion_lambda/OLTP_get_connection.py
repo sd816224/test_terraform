88a3965b38c384ddd8fc87127579a8094a9c125e
Alex Chan <alextfchan96@gmail.com>
2023-11-02 11:41:17 +0000

    working on comprehensive error testing. updated get_connection util to refer to the get_credentials util

--- src/ingestion_lambda/OLTP_get_connection.py
+++ src/ingestion_lambda/OLTP_get_connection.py
@@ -1,24 +1,51 @@
-# import os
 import logging
-from pg8000.native import Connection, InterfaceError, DatabaseError, Error
-# from dotenv import load_dotenv
+from pg8000 import Connection, DatabaseError, InterfaceError
 
 
 logger = logging.getLogger('MyLogger')
 logger.setLevel(logging.INFO)
 
-def get_connection(user, host, database, port, password):
+
+def get_connection(database_credentials):
     '''
-    This utility function allows for connections to the OLTP database - Totesys.
+    Gets connections to the OLTP database - Totesys.
+
+
+    Parameters
+    ----------
+    database_credentials (from the get_credentials util) - which is a dictionary consisting of:
+        user
+        host
+        database
+        port
+        password
+
+    Raises
+    ------
+    DatabaseError: Will return an error message showing what is missing.
+        i.e. if the password is wrong, the error message will state password 
+        authentication has failed.
+    InterfaceError: 
+
+    Returns
+    -------
+    A successful pg8000 connection object will be returned.
     '''
     try:
-        load_dotenv()
+        user=database_credentials['user']
+        host=database_credentials['host']
+        database=database_credentials['database']
+        port=database_credentials['port']
+        password=database_credentials['password']
+        conn= Connection(user, host, database, port, password, timeout=1)
         logger.info('Connection to database Totesys has been established.')
-        return Connection(user, host, database, port, password)
-
-    except (InterfaceError, DatabaseError) as e:
-        logger.error('pg8000 - an error has occured.')
-        raise e
+        return conn
+    except DatabaseError as db:
+        logger.error(f"pg8000 - an error has occured: {db.args[0]['M']}")
+        raise db
+    except InterfaceError as ie:
+        logger.error(f'pg8000 - an error has occured: \n"{ie}"')
+        raise ie
     except Exception as exc:
         logger.error('An error has occured when attempting to connect to the database.')
         raise exc
