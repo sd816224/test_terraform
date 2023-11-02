f88806e2cccdda9b1cb644927c4a6767b4971a79
pavzari <92022046+pavzari@users.noreply.github.com>
2023-11-02 10:28:44 +0000

    added ingestion lambda code zip and layer reference

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,3 +1,15 @@
 data "aws_caller_identity" "current"{}
 
 data "aws_region" "current"{}
+
+data "archive_file" "ingestion_lambda_code_zip" {
+  type        = "zip"
+  source_file = "${path.module}/../src/ingestion_lambda/ingestion_lambda.py"
+  output_path = "${path.module}/../src/ingestion_lambda/ingestion_lambda.zip"
+}
+
+resource "aws_s3_object" "ingestion_lambda_code_upload" {
+  bucket = aws_s3_bucket.lambda_code_bucket.id
+  key    = "ingestion_lambda/ingestion_lambda.zip"
+  source = data.archive_file.ingestion_lambda_code_zip.output_path
+}
