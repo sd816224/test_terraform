a533f840500b26c7fec1cffeaf002beffc0b0f35
Josh Lee <joshlee@Joshs-MacBook-Pro.local>
2023-11-02 11:44:36 +0000

    completed get load upload and testing

--- src/ingestion_lambda/get_last_upload.py
+++ src/ingestion_lambda/get_last_upload.py
@@ -2,29 +2,39 @@ import boto3
 from datetime import date
 from datetime import timedelta
 from datetime import datetime as dt
+from botocore.exceptions import ClientError
+import logging
+
+logger = logging.getLogger("MyLogger")
+logger.setLevel(logging.INFO)
 
 
 def get_last_upload(bucket_name):
     """retrieves the time the s3 bucket was modified
 
     returns a datetime object that can be used in a psql query
+    if no last update file exists it returns a default datetime object
 
     Args:
         bucket name
 
     Raises:
-        ???"""
+        clientError"""
 
-    yesterday = date.today() - timedelta(days=1)
-    year = yesterday.year
-    month = yesterday.month
-    day = yesterday.day
+    client = boto3.client("s3")
 
-    s3 = boto3.resource("s3")
-    bucket = s3.Bucket(bucket_name)
+    try:
+        response = client.get_object(Bucket=bucket_name, Key="last_update.txt")
+        datetime_string = response["Body"].read().decode("utf-8")
+        datetime_object = dt.strptime(datetime_string, "%Y:%m:%d:%H:%M:%S")
 
-    objs = [
-        obj.last_modified.strftime("%Y:%m:%d:%H:%M:%S")
-        for obj in bucket.objects.filter(Prefix=yesterday.strftime("%Y/%m/%d/"))
-    ]
-    print(objs)
+        return datetime_object
+    except ClientError as e:
+        if e.response["Error"]["Message"] == "The specified key does not exist.":
+            datetime_object = dt.strptime("2020:1:1:00:00:00", "%Y:%m:%d:%H:%M:%S")
+            logger.info("default datetime object returned")
+            return datetime_object
+        else:
+            logger.error(e.response["Error"]["Message"])
+    except Exception as e:
+        logger.error(f"An unexpected error occured {e}")
--- test/test_get_last_upload.py
+++ test/test_get_last_upload.py
@@ -4,7 +4,8 @@ import pytest
 import os
 import boto3
 from pprint import pprint
-import time
+from datetime import datetime as dt
+import logging
 
 
 @pytest.fixture(scope="function")
@@ -23,10 +24,48 @@ class TestGetLastUpload:
             Bucket="TestBucket",
             CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
         )
-        client.put_object(Body="stuff", Key="2023/11/1/stuff1", Bucket="TestBucket")
-        time.sleep(2)
-        client.put_object(Body="stuff", Key="2023/11/1/stuff2", Bucket="TestBucket")
-        time.sleep(2)
-        client.put_object(Body="stuff", Key="2023/10/31/stuff3", Bucket="TestBucket")
+        client.put_object(
+            Body="2023:11:2:00:00:00", Key="last_update.txt", Bucket="TestBucket"
+        )
+        result = get_last_upload("TestBucket")
+        assert result == dt(2023, 11, 2, 0, 0, 0)
 
-        get_last_upload("TestBucket")
+    def test_returns_default_datetime_if_no_last_update_exists(self):
+        client = boto3.client("s3")
+        client.create_bucket(
+            Bucket="TestBucket",
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
+        client.put_object(
+            Body="2023:11:2:00:00:00", Key="not_a_file.txt", Bucket="TestBucket"
+        )
+        result = get_last_upload("TestBucket")
+        assert result == dt(2020, 1, 1, 0, 0, 0)
+
+    def test_logs_when_default_datetime_is_returned(self, caplog):
+        with caplog.at_level(logging.INFO):
+            client = boto3.client("s3")
+            client.create_bucket(
+                Bucket="TestBucket",
+                CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+            )
+            client.put_object(
+                Body="2023:11:2:00:00:00", Key="not_a_file.txt", Bucket="TestBucket"
+            )
+            get_last_upload("TestBucket")
+
+            assert "default datetime object returned" in caplog.text
+
+    def test_logs_client_error_when_invalid_bucket_name(self, caplog):
+        with caplog.at_level(logging.INFO):
+            client = boto3.client("s3")
+            client.create_bucket(
+                Bucket="TestBucket",
+                CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+            )
+            client.put_object(
+                Body="2023:11:2:00:00:00", Key="not_a_file.txt", Bucket="TestBucket"
+            )
+            get_last_upload("NotABucket")
+
+            assert "The specified bucket does not exist" in caplog.text
