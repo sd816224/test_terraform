db10a776d2ab3692536e7badaa73f46934320079
pavzari <92022046+pavzari@users.noreply.github.com>
2023-11-02 15:45:57 +0000

    ..

--- src/ingestion_lambda/ingestion_lambda.py
+++ src/ingestion_lambda/ingestion_lambda.py
@@ -1,2 +1,12 @@
-def lambda_handler():
+def lambda_handler(event, context):
+    credentials = get_credentials("totesys-production")
+
+    connection = get_connetion(credentials)
+
+    data = get_data(connection)
+
+    get_last_upload("")
+
+    write_file
+
     pass
