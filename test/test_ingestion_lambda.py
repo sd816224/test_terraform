a92bb4aad793fe4246d54f7e98f57dd8b077a785
Josh Lee <joshlee@Joshs-MacBook-Pro.local>
2023-11-02 17:14:53 +0000

    compiled handler and tested using my own aws credentials - successfully got all of the data from the db in the required format saved into an s3 bucket - just need to fix the where clause in the SQL query to account for the time period

---
+++ test/test_ingestion_lambda.py
@@ -0,0 +1,12 @@
+from src.ingestion_lambda.ingestion_lambda import lambda_handler
+from moto import mock_s3
+from datetime import datetime as dt
+import boto3
+import pytest
+import os
+import time_machine
+import logging
+
+
+def test_it_works():
+    lambda_handler("event", "context")
