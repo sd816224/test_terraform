016b28ed3bdd60aee05c8da2722c22048abd583b
Sam Jukes <samjukes.dev@gmail.com>
2023-11-03 17:28:04 +0000

    added the bucket to the resource as well as objects within bucket

--- terraform/iam_ingestion.tf
+++ terraform/iam_ingestion.tf
@@ -1,21 +1,21 @@
 resource "aws_iam_role" "role_for_ingestion_lambda" {
-  name               = "role_for_ingestion_lambda"
+  name = "role_for_ingestion_lambda"
   assume_role_policy = jsonencode({
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Action": [
-                    "sts:AssumeRole"
-                ],
-                "Principal": {
-                    "Service": [
-                        "lambda.amazonaws.com"
-                    ]
-                }
-            }
-        ]
-    })
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "sts:AssumeRole"
+        ],
+        "Principal" : {
+          "Service" : [
+            "lambda.amazonaws.com"
+          ]
+        }
+      }
+    ]
+  })
 }
 
 
@@ -31,7 +31,7 @@ resource "aws_iam_policy" "cloudwatch_logs_policy_for_ingestion_lambda" {
         Resource = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"]
       },
       {
-        Action   = ["logs:CreateLogStream","logs:PutLogEvents"],
+        Action   = ["logs:CreateLogStream", "logs:PutLogEvents"],
         Effect   = "Allow",
         Resource = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${var.ingestion_lambda}:*"]
       }
@@ -46,8 +46,8 @@ resource "aws_iam_policy" "ingestion_lambda_s3_code_bucket_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow",
-        Action = "s3:GetObject",
+        Effect   = "Allow",
+        Action   = "s3:GetObject",
         Resource = ["${aws_s3_bucket.lambda_code_bucket.arn}/*"]
       }
     ]
@@ -62,8 +62,14 @@ resource "aws_iam_policy" "ingestion_lambda_s3_ingestion_bucket_policy" {
     Statement = [
       {
         Effect = "Allow",
-        Action = ["s3:GetObject", "s3:PutObject"],
-        Resource = ["${aws_s3_bucket.ingestion_data_bucket.arn}/*"]
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ],
+        Resource = [
+          "${aws_s3_bucket.ingestion_data_bucket.arn}/*",
+          "${aws_s3_bucket.ingestion_data_bucket.arn}"
+        ]
       }
     ]
   })
