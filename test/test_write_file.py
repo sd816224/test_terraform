57bc38ba4b606b7a3b4a457c87a1c7b3f39f3870
pavzari <92022046+pavzari@users.noreply.github.com>
2023-11-06 15:19:35 +0000

    test fixes for write_file function

--- test/test_write_file.py
+++ test/test_write_file.py
@@ -24,49 +24,53 @@ class TestWriteFile:
     """tests for write file util function"""
 
     def test_puts_json_file_and_updated_file_in_s3_bucket(self):
-        testJSON = '{"name": "John", "age": 30, "city": "New York"}'
+        testJSON = {"name": "John", "age": 30, "city": "New York"}
+        timestamp = dt.now()
         s3 = boto3.client("s3")
         s3.create_bucket(
             Bucket="TestBucket",
             CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
         )
-        write_file("TestBucket", testJSON)
+        write_file("TestBucket", testJSON, timestamp)
 
         response = s3.list_objects(Bucket="TestBucket")
-        assert len(response["Contents"]) == 2
+        assert len(response["Contents"]) == 4
 
     @time_machine.travel(dt(2020, 1, 1, 17, 30, 19))
     def test_key_is_correct(self):
-        testJSON = '{"name": "John", "age": 30, "city": "New York"}'
+        testJSON = {"name": "John", "age": 30, "city": "New York"}
+        timestamp = dt.now()
         s3 = boto3.client("s3")
         s3.create_bucket(
             Bucket="TestBucket",
             CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
         )
-        write_file("TestBucket", testJSON)
+        write_file("TestBucket", testJSON, timestamp)
 
         response = s3.list_objects(Bucket="TestBucket")
-        assert response["Contents"][0]["Key"] == "2020/1/1/data-173019.json"
+        assert response["Contents"][0]["Key"] == "age/2020/1/1/data-173019.json"
 
     @time_machine.travel(dt(2020, 1, 1, 17, 30, 19))
     def test_successful_log_output_is_correct(self, caplog):
         with caplog.at_level(logging.INFO):
-            testJSON = '{"name": "John", "age": 30, "city": "New York"}'
+            testJSON = {"name": "John", "age": 30, "city": "New York"}
+            timestamp = dt.now()
             s3 = boto3.client("s3")
             s3.create_bucket(
                 Bucket="TestBucket",
                 CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
             )
-            write_file("TestBucket", testJSON)
+            write_file("TestBucket", testJSON, timestamp)
 
             assert (
-                "Success. File 2020/1/1/data-173019.json saved." in caplog.text
+                "Success. File age/2020/1/1/data-173019.json saved." in caplog.text
             )  # noqa: E501
 
     def test_raises_client_error_if_invalid_bucket_name(self, caplog):
         with caplog.at_level(logging.ERROR):
-            testJSON = '{"name": "John", "age": 30, "city": "New York"}'
-            write_file("TestBucket", testJSON)
+            testJSON = {"name": "John", "age": 30, "city": "New York"}
+            timestamp = dt.now()
+            write_file("TestBucket", testJSON, timestamp)
 
             assert "The specified bucket does not exist" in caplog.text
 
@@ -82,22 +86,25 @@ class TestWriteFile:
 
             assert "No JSON data provided" in caplog.text
 
+    @time_machine.travel(dt(2020, 1, 1, 17, 30, 19))
     def test_writes_file_with_correct_time_stamp(self):
-        testJSON = '{"name": "John", "age": 30, "city": "New York"}'
+        testJSON = {"name": "John", "age": 30, "city": "New York"}
+        timestamp = dt.now()
         s3 = boto3.client("s3")
         s3.create_bucket(
             Bucket="TestBucket",
             CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
         )
-        write_file("TestBucket", testJSON)
+        write_file("TestBucket", testJSON, timestamp)
         response = s3.get_object(Bucket="TestBucket", Key="last_update.txt")
         text = response["Body"].read().decode("utf-8")
 
-        assert text == "2020:01:01%:00:00:00"
+        assert text == "2020:01:01:17:30:19"
 
-    def test_handles_key_error_if_string_json_not_provided(self, caplog):
+    def test_handles_key_error_if_json_not_provided(self, caplog):
         with caplog.at_level(logging.ERROR):
-            testJSON = {"name": "John", "age": 30, "city": "New York"}
-            write_file("TestBucket", testJSON)
+            testJSON = '{"name": "John", "age": 30, "city": "New York"}'
+            timestamp = dt.now()
+            write_file("TestBucket", testJSON, timestamp)
 
-            assert "Invalid type for parameter Body" in caplog.text
+            assert "Updated data type invalid. Dict expected." in caplog.text
