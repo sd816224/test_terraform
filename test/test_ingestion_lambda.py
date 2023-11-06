08a129d24a67d408a17377fb170688852947a79c
Josh Lee <joshlee@Joshs-MacBook-Pro.local>
2023-11-06 12:22:32 +0000

    updated ingestion lambda to save each table results if different files

--- test/test_ingestion_lambda.py
+++ test/test_ingestion_lambda.py
@@ -1,13 +1,13 @@
-# from src.ingestion_lambda.ingestion_lambda import lambda_handler
-# from moto import mock_s3
-# from datetime import datetime as dt
+from src.ingestion_lambda.ingestion_lambda import lambda_handler
+from moto import mock_s3
+from datetime import datetime as dt
 
-# import boto3
-# import pytest
-# import os
-# import time_machine
-# import logging
+import boto3
+import pytest
+import os
+import time_machine
+import logging
 
 
-# def test_it_works():
-#     lambda_handler("event", "context")
+def test_it_works():
+    lambda_handler("event", "context")
