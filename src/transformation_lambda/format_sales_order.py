04967d120f425aff4a9e10463c2f00a5ba90ecc2
Sam Jukes <samjukes.dev@gmail.com>
2023-11-06 16:55:10 +0000

    passes flake8

--- src/transformation_lambda/format_sales_order.py
+++ src/transformation_lambda/format_sales_order.py
@@ -7,27 +7,25 @@ def format_sales_order(sales_order_json):
         created_time = sale["created_at"][11:19]
         last_updated_date = sale["last_updated"][:10]
         last_updated_time = sale["last_updated"][11:19]
-        row =  [
-                    sale["sales_order_id"],
-                    created_date,
-                    created_time,
-                    last_updated_date,
-                    last_updated_time,
-                    sale["staff_id"],
-                    sale["counterparty_id"],
-                    sale["units_sold"],
-                    sale["unit_price"],
-                    sale["currency_id"],
-                    sale["design_id"],
-                    sale["agreed_payment_date"],
-                    sale["agreed_delivery_date"],
-                    sale["agreed_delivery_location_id"],
-                ]
+        row = [
+            sale["sales_order_id"],
+            created_date,
+            created_time,
+            last_updated_date,
+            last_updated_time,
+            sale["staff_id"],
+            sale["counterparty_id"],
+            sale["units_sold"],
+            sale["unit_price"],
+            sale["currency_id"],
+            sale["design_id"],
+            sale["agreed_payment_date"],
+            sale["agreed_delivery_date"],
+            sale["agreed_delivery_location_id"],
+        ]
         for list in sales_order_parquet:
             if list == row:
                 insert = False
-        if insert == True:
+        if insert is True:
             sales_order_parquet.append(row)
     return sales_order_parquet
-
-
