040770509481039d3e0f0beb68e9a5c61f69f595
Sam Jukes <samjukes.dev@gmail.com>
2023-11-06 16:47:36 +0000

    format_sales_order fully tested and passing

---
+++ src/transformation_lambda/format_sales_order.py
@@ -0,0 +1,33 @@
+def format_sales_order(sales_order_json):
+    json = sales_order_json["sales_order"]
+    sales_order_parquet = []
+    for sale in json:
+        insert = True
+        created_date = sale["created_at"][:10]
+        created_time = sale["created_at"][11:19]
+        last_updated_date = sale["last_updated"][:10]
+        last_updated_time = sale["last_updated"][11:19]
+        row =  [
+                    sale["sales_order_id"],
+                    created_date,
+                    created_time,
+                    last_updated_date,
+                    last_updated_time,
+                    sale["staff_id"],
+                    sale["counterparty_id"],
+                    sale["units_sold"],
+                    sale["unit_price"],
+                    sale["currency_id"],
+                    sale["design_id"],
+                    sale["agreed_payment_date"],
+                    sale["agreed_delivery_date"],
+                    sale["agreed_delivery_location_id"],
+                ]
+        for list in sales_order_parquet:
+            if list == row:
+                insert = False
+        if insert == True:
+            sales_order_parquet.append(row)
+    return sales_order_parquet
+
+
