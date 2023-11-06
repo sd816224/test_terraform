adb133428b076d5ff47341362f5dc30d2aba57ec
Sam Jukes <samjukes.dev@gmail.com>
2023-11-06 09:40:39 +0000

    update the timeout to make room for lengthy runtime

--- terraform/lambda_ingestion.tf
+++ terraform/lambda_ingestion.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "ingestion_lambda" {
   function_name    = var.ingestion_lambda
   handler          = "ingestion_lambda.lambda_handler" 
   runtime          = "python3.11"
-  timeout          = 30
+  timeout          = 900
   role             = aws_iam_role.role_for_ingestion_lambda.arn
   s3_bucket        = aws_s3_bucket.lambda_code_bucket.id
   s3_key           = "ingestion_lambda/ingestion_lambda.zip"
