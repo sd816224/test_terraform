a9662c4153f65eda0fac06e0854148a45660fdfe
sd816224 <ufoczl@yahoo.co.uk>
2023-11-08 13:52:26 +0000

    terraform transformation done.............maybe

--- src/ingestion_lambda/ingestion_lambda.py
+++ src/ingestion_lambda/ingestion_lambda.py
@@ -14,7 +14,8 @@ logger.setLevel(logging.INFO)
 def lambda_handler(event, context):
     invocation_time = dt.now()
     bucket_name = (
-        "nc-de-project-ingested-data-bucket-20231108101054774200000001"  # noqa E501
+        #change name of your bucket!!!!!!!!!!!!!!!!!!!!!
+        "hange name of your bucket!!!!!!!!!!!!!!!!!!!!!"  # noqa E501
     )
     secret_name = "totesys-production"
 
