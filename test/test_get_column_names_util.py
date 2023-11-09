6146db4b41072797e2f50d1ad99ec338768e4688
Josh Lee <joshlee@Joshs-MacBook-Pro.local>
2023-11-09 17:05:24 +0000

    fixed flake8 issues on get_column_names testing

--- test/test_get_column_names_util.py
+++ test/test_get_column_names_util.py
@@ -16,8 +16,8 @@ def test_real_dim_counterparty_columns():
     result = get_column_names(conn, "dim_counterparty")
     assert (
         result
-        == "('counterparty_id', 'counterparty_legal_name', 'counterparty_legal_address_line_1', 'counterparty_legal_address_line_2', 'counterparty_legal_district', 'counterparty_legal_city', 'counterparty_legal_postal_code', 'counterparty_legal_country', 'counterparty_legal_phone_number')"
-    )  # noqa E501
+        == "('counterparty_id', 'counterparty_legal_name', 'counterparty_legal_address_line_1', 'counterparty_legal_address_line_2', 'counterparty_legal_district', 'counterparty_legal_city', 'counterparty_legal_postal_code', 'counterparty_legal_country', 'counterparty_legal_phone_number')"  # noqa E501
+    )
 
 
 def test_real_dim_currency_columns():
@@ -53,8 +53,8 @@ def test_real_dim_location_columns():
     result = get_column_names(conn, "dim_location")
     assert (
         result
-        == "('location_id', 'address_line_1', 'address_line_2', 'district', 'city', 'postal_code', 'country', 'phone')"
-    )  # noqa E501
+        == "('location_id', 'address_line_1', 'address_line_2', 'district', 'city', 'postal_code', 'country', 'phone')"  # noqa E501
+    )
 
 
 def test_real_dim_staff_columns():
@@ -64,8 +64,8 @@ def test_real_dim_staff_columns():
     result = get_column_names(conn, "dim_staff")
     assert (
         result
-        == "('staff_id', 'first_name', 'last_name', 'department_name', 'location', 'email_address')"
-    )  # noqa E501
+        == "('staff_id', 'first_name', 'last_name', 'department_name', 'location', 'email_address')"  # noqa E501
+    )
 
 
 def test_real_dim_currency_columns_incorrect_table_name(caplog):
