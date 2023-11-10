adbdf31bef67b2e44119ae38324818d61b93620c
pavzari <92022046+pavzari@users.noreply.github.com>
2023-11-10 10:09:47 +0000

    test fixes for dim date and get column name

--- src/transformation_lambda/format_dim_date.py
+++ src/transformation_lambda/format_dim_date.py
@@ -1,12 +1,12 @@
 from datetime import datetime as dt
 import logging
 
-logger = logging.getLogger('MyLogger')
+logger = logging.getLogger("MyLogger")
 logger.setLevel(logging.INFO)
 
 
 def format_dim_date(sales_order_table):
-    '''
+    """
     argument:sales_order_table
         type:dict
             key: table name
@@ -34,18 +34,20 @@ def format_dim_date(sales_order_table):
         do we need to avoid it? how?
 
         im guessing the logic all done by loading lambda maybe
-    '''
+    """
     try:
         # read updated content from input table
-        content = sales_order_table['sales_order']
+        content = sales_order_table["sales_order"]
 
         # extract all date objects from table
         all_dates = []
         for row in content:
-            all_dates += [dt.strptime(row['agreed_delivery_date'], '%Y-%m-%d'),
-                          dt.strptime(row['agreed_payment_date'], '%Y-%m-%d'),
-                          dt.strptime(row['created_at'][:10], '%Y-%m-%d'),
-                          dt.strptime(row['last_updated'][:10], '%Y-%m-%d')]
+            all_dates += [
+                dt.strptime(row["agreed_delivery_date"], "%Y-%m-%d"),
+                dt.strptime(row["agreed_payment_date"], "%Y-%m-%d"),
+                dt.strptime(row["created_at"][:10], "%Y-%m-%d"),
+                dt.strptime(row["last_updated"][:10], "%Y-%m-%d"),
+            ]
 
         # remove the duplicate date
         remove_duplicate = list(set(all_dates))
@@ -53,34 +55,38 @@ def format_dim_date(sales_order_table):
         # formatting
         result = []
         for date in remove_duplicate:
-            result.append({
-                'date_id': date.strftime('%Y-%m-%d'),
-                'year': date.year,
-                'month': date.month,
-                'day': date.day,
-                'day_of_week': date.weekday(),
-                'day_name': date.strftime('%A'),
-                'month_name': date.strftime('%B'),
-                'quarter': 1 + (dt(2020, 9, 1).month - 1) // 3,
-            })
+            result.append(
+                {
+                    "date_id": date.strftime("%Y-%m-%d"),
+                    "year": date.year,
+                    "month": date.month,
+                    "day": date.day,
+                    "day_of_week": date.weekday(),
+                    "day_name": date.strftime("%A"),
+                    "month_name": date.strftime("%B"),
+                    "quarter": 1 + (date.month - 1) // 3,
+                }
+            )
 
         # transfer to list of list
         list_list = []
         for row in result:
-            list_list.append([
-                row['date_id'],
-                row['year'],
-                row['month'],
-                row['day'],
-                row['day_of_week'],
-                row['day_name'],
-                row['month_name'],
-                row['quarter'],
-                ])
+            list_list.append(
+                [
+                    row["date_id"],
+                    row["year"],
+                    row["month"],
+                    row["day"],
+                    row["day_of_week"],
+                    row["day_name"],
+                    row["month_name"],
+                    row["quarter"],
+                ]
+            )
         return list_list
 
     except KeyError as k:
-        logger.error(f'Error retrieving data, {k}')
+        logger.error(f"Error retrieving data, {k}")
 
     except Exception as e:
         logger.error(e)
