09731b36e3395b51f9e2e67eb7c5e4cc7c63183d
sd816224 <ufoczl@yahoo.co.uk>
2023-11-10 16:04:42 +0000

    test jq work on workflow

--- terraform/test.sh
+++ terraform/test.sh
@@ -4,14 +4,15 @@
 # abc='hello'
 # echo ${abc}
 
-if aws s3api get-object \
---bucket nc-project-backend20231107140228968000000001 \
---key production/terraform.tfstate \
- test.json
+# if aws s3api get-object \
+# --bucket nc-project-backend20231107140228968000000001 \
+# --key production/terraform.tfstate \
+#  test.json
 
-then
-echo 'hello'
-else
+# then
+# echo $(jq '.check_results' test.json)
+# else
 
-fi
+# fi
 
+echo $(jq '.resources' test.json)
