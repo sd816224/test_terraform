2be98fa9eab830d0294c0d5877b7ad74b87b2976
sd816224 <ufoczl@yahoo.co.uk>
2023-11-10 17:53:51 +0000

    test terraform create data bucket

---
+++ terraform/init_buckets.tf
@@ -0,0 +1,5 @@
+module "nc_project_init_bucket_module"{
+  source="terraform-aws-modules/s3-bucket/aws"
+
+  bucket_prefix="data-bucket-iii"
+}
--- terraform/test.sh
+++ terraform/test.sh
@@ -7,7 +7,7 @@
 # if aws s3api get-object \
 # --bucket nc-project-backend20231107140228968000000001 \
 # --key production/terraform.tfstate \
-#  test.json
+#  instances.json
 
 # then
 # echo $(jq '.check_results' test.json)
@@ -15,4 +15,48 @@
 
 # fi
 
-echo $(jq '.resources[0].instances' test.json)
+# echo $(jq '.resources[0].instances' test.json)
+
+
+# terraform apply -target=module.nc_project_init_bucket_module --auto-approve 
+
+#### checn json variable exist
+# xxx=$(jq '.resources[0].instances' emptytf.json)
+# echo "$xxx"
+
+# if [jq -e . >/dev/null 2>&1 <<<"$xxx"] ; then
+#     echo "Parsed JSON successfully and got something other than false/null"
+# else
+#     echo "Failed to parse JSON, or got false/null"
+# fi
+
+# echo $(jq '.resources[0].instances' emptytf.json) 
+
+
+####check if document exist
+
+# if aws s3api get-object \
+# --bucket nc-project-backend20231107140228968000000001 \
+# --key production/terraform.tfstate \
+#  instances.json
+# then 
+#     xxx=$(jq '.resources[0].instances' instances.json)
+#     if [jq -e . >/dev/null 2>&1 <<<"$xxx"] ; then
+#         echo "bucket created already, instances file is ready"
+#     else
+#         echo 'create bucket here:'
+#         terraform apply -target=module.nc_project_init_bucket_module --auto-approve 
+#     fi
+# else 
+# terraform init
+# terraform apply -target=module.nc_project_init_bucket_module --auto-approve 
+# fi
+
+# terraform apply -target=module.nc_project_init_bucket_module -auto-approve 
+# aws s3api get-object \
+# --bucket nc-project-backend20231107140228968000000001 \
+# --key production/terraform.tfstate \
+#  instances.json
+
+
+cat ./test.json
