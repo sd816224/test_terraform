dedac0f70adae768274e56b2a7881272f50afdf7
Sam Jukes <samjukes.dev@gmail.com>
2023-11-10 09:30:02 +0000

    no more regex util

--- src/loading_lambda/regex_util.py
+++
@@ -1,2 +0,0 @@
-        for column in restored_df.columns:
-        restored_df[column] = restored_df[column].map(lambda x: x.replace("'", "''") if isinstance(x, str) else x)
--- test/test_regex_util.py
+++
@@ -1,6 +0,0 @@
-from src.loading_lambda.regex_util import escape_apostrophe
-
-def test_test():
-    assert escape_apostrophe("O'Brien") == "O''Brien"
-
-    
