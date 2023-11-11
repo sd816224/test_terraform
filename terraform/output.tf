ee0a4a9c470b41d1c7620d1e163b81cbc6d32d26
sd816224 <ufoczl@yahoo.co.uk>
2023-11-11 00:22:19 +0000

    revise output from arn to id

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,4 @@
-output "data_bucket_arn"{
-  description="created bucket name of backend"
-  value=module.nc_project_init_bucket_module.s3_bucket_arn
+output "data_bucket_name"{
+  description="created data bucket"
+  value=module.nc_project_init_bucket_module.s3_bucket_id
 }
