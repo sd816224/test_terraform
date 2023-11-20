9611f57349d400f4f6dfe679bee0e989b33fe69a
sd816224 <107551749+sd816224@users.noreply.github.com>
2023-11-20 19:49:48 +0000

    Update event_ingestion.tf

--- terraform/event_ingestion.tf
+++ terraform/event_ingestion.tf
@@ -8,10 +8,10 @@ resource "aws_cloudwatch_event_target" "ingestion_lambda_event_target" {
   arn  = aws_lambda_function.ingestion_lambda.arn
 }
 
-resource "aws_lambda_permission" "allow_scheduler" {
-  statement_id   = "AllowExecutionFromEventBridge"
-  action         = "lambda:InvokeFunction"
-  function_name  = aws_lambda_function.ingestion_lambda.function_name
-  principal      = "events.amazonaws.com"
-  source_arn     = aws_cloudwatch_event_rule.event_bridge_invoke_ingestion_lambda_rule.arn
-}
+#resource "aws_lambda_permission" "allow_scheduler" {
+#  statement_id   = "AllowExecutionFromEventBridge"
+#  action         = "lambda:InvokeFunction"
+#  function_name  = aws_lambda_function.ingestion_lambda.function_name
+#  principal      = "events.amazonaws.com"
+#  source_arn     = aws_cloudwatch_event_rule.event_bridge_invoke_ingestion_lambda_rule.arn
+#}
